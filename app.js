c2473f93683040e1d5ee12b138247c1b9bbd62fa
Kathryn <kathryn@Kathryns-MacBook-Pro.local>
2024-03-28 15:32:40 -0500

    the subheader disappears and reappears, new folders added, github and linkedin links added

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
+// header
+let getMeSubtitle = document.getElementById("meSubtitle");
 
+// project panel
 let getProjectsPanel = document.getElementById("projectsPanelContents");
-let getContactPanelContents = document.getElementById("contactPanelContents");
-let getInnerProjectFrame = document.getElementById("projectContentsFrame");
-let getWaluigiContents = document.getElementById("waluigiContents");
-let getMemoryContents = document.getElementById("memoryContents");
+
+// folder screen
 let getFolders = document.getElementById("projectCardFrame");
+
+// inside the folder
+let getInnerProjectFrame = document.getElementById("projectContentsFrame");
+
+// inner folder template
 let getTitle = document.getElementById("mainTitle");
 let getSubtitle = document.getElementById("mainSubtitle");
 let getBulletPoints = document.getElementById("mainBulletPoints");
 let getLink = document.getElementById("mainLink");
-let getMeDescriptor = document.getElementById("meDescriptor");
+
+// inner folder project specific (only one of these shows)
+let getWaluigiContents = document.getElementById("waluigiContents");
+let getMemoryContents = document.getElementById("memoryContents");
+
+// content panel
+let getContactPanelContents = document.getElementById("contactPanelContents");
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~ header ~~~~~~~~~~~~~~~~~~~~
+function hideMeSubtitle() { // hide my header subtitle (hidden when a panel is open)
+  getMeSubtitle.style.display = "none";
+  console.log("hide me subtitle")
+}
+
+function showMeSubtitle() { // show my header subtitle (only visible when panels are closed)
+  setTimeout (() => {
+  getMeSubtitle.style.display = "flex";
+  }, 300)  
+  
+}
+
+// ~~~~~~~~~~~~~~~~~~~~ project panel ~~~~~~~~~~~~~~~~~~~~
 
 function closePanels() {
    if (getContactPanelContents.classList.contains("visible")) {closeContactPanelContents()}
-   if (getProjectsPanel.classList.contains("visible")) {closeProjectsPanelContents()} 
+   if (getProjectsPanel.classList.contains("visible")) {closeProjectsPanelContents()}
+  showMeSubtitle();
 }
 
 function openProjectsPanelContents() {
@@ -24,28 +55,20 @@ function openProjectsPanelContents() {
   }
   //   if .PPC is hidden, change .ProjectsPanelContents display to visible
   if (getProjectsPanel.classList.contains("hidden")) {
-    // getProjectsPanel.style.display = "flex";
-       getProjectsPanel.classList.remove("hidden");
+    getProjectsPanel.classList.remove("hidden");
     getProjectsPanel.classList.add("visible");
     // when you open the projects panel the folders frame will automatically appear after 1s
-    // setTimeout (() => {
-      getFolders.classList.remove("hidden")
+    getFolders.classList.remove("hidden")
     getFolders.classList.add("visible")
-    // }, 500);
-//     make the excess page shrink
-    // document.getElementById("shrink").style.flexGrow = ".001";
-    // document.getElementById("shrink").style.maxHeight = "0px";
-//     hide descriptor
-    getMeDescriptor.style.display = "none";
+    hideMeSubtitle();
   } else {
    closeProjectsPanelContents();
+   showMeSubtitle();
   }
 }
 function closeProjectsPanelContents() {
     getProjectsPanel.classList.remove("visible");
     getProjectsPanel.classList.add("hidden");
-      //     make the excess page grow
-    // document.getElementById("shrink").style.flexGrow = "1";
 //   closing the inner project frame
   if (getInnerProjectFrame.classList.contains('visible')) {
     getInnerProjectFrame.classList.remove("visible")
@@ -58,6 +81,88 @@ function closeProjectsPanelContents() {
   }
 }
 
+// ~~~~~~~~~~~~~~~~~~~~ folder screen ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function showFolders() {
+  console.log("go back to folder screen");
+  setTimeout (() => {
+  getFolders.classList.remove("hidden");
+  getFolders.classList.add("visible");
+          }, 300)
+  getInnerProjectFrame.classList.remove('visible');
+  getInnerProjectFrame.classList.add("hidden");
+}
+
+function hideFolders() {
+ console.log("hiding folders");
+ getFolders.classList.remove("visible");
+ getFolders.classList.add("hidden");
+}
+// ~~~~~~~~~~~~~~~~~~~~ inner folders ~~~~~~~~~~~~~~~~~~~~
+
+function showProjectContents() {
+  console.log("showing content frame")    
+  getInnerProjectFrame.classList.remove('hidden');
+  getInnerProjectFrame.classList.add("visible");
+}
+
+function folderOpeningGeneral() { // the general things that happen when you open a folder,
+  // gets called by another function that changes the template's contents
+  hideFolders();
+  setTimeout (() => {
+showProjectContents();
+    }, 100)
+}
+
+function openWaluigiContents() {
+  folderOpeningGeneral();
+  getTitle.innerHTML = "Waluigi Jail";
+  getSubtitle.innerHTML = "This is an introductory web development project intended to develop HTML, CSS, and JavaScript skills. Users interact with the central image, triggering dynamic transformations and text display with each click, demonstrating foundational coding proficiency and creativity."
+  getBulletPoints.innerHTML = "123";
+  getLink.setAttribute("href", "kathryn.mckean.github.io/waluigijail/");
+}
+
+
+function openMemoryContents() {
+  folderOpeningGeneral();
+  getTitle.innerHTML = "Memory Game";
+  getSubtitle.innerHTML = "This is a simple project centered around JavaScript, focusing on state management and the utilization of for loops."
+  getBulletPoints.innerHTML = "1234";
+  getLink.setAttribute("href", "kathryn.mckean.github.io/memorygame/");
+}
+
+function openNoteContents() {
+  folderOpeningGeneral();
+  getTitle.innerHTML = "Note Taking";
+  getSubtitle.innerHTML = "Develops CRUD skills through a simple note making page. You can create, read, update, and delete notes.";
+  getBulletPoints.innerHTML = "the skills developed";
+  getLink.setAttribute("href", "kathryn.mckean.github.io/noteTaker/")
+}
+
+function openPetContents() {
+  folderOpeningGeneral();
+  getTitle.innerHTML = "Michelle at your Service";
+  getSubtitle.innerHTML = "A webpage for a local petcare company. Utilized Figma to design the webpage.";
+  getBulletPoints.innerHTML = "Figma, etc";
+  getLink.setAttribute("href", "kathryn.mckean.github.io/MAYS/")
+}
+function openFortuneContents() { 
+  folderOpeningGeneral();
+  getTitle.innerHTML = "giFortune Teller";
+  getSubtitle.innerHTML = "Using the giphy API, your fortune is told. The user inputs their question and a related gif is shown. Like a giphy search bar, but more interesting";
+  getBulletPoints.innerHTML = "Pro/Con";
+  getLink.setAttribute("href", "kathryn.mckean.github.io/gifFortune/")
+}
+
+// function openContentsTemplate() { fill in info for specific project
+//   folderOpeningGeneral();
+//   getTitle.innerHTML = "";
+//   getSubtitle.innerHTML = "";
+//   getBulletPoints.innerHTML = "";
+//   getLink.setAttribute("href", "kathryn.mckean.github.io/~~~/")
+// }
+
+
+// ~~~~~~~~~~~~~~~~~~~~ contact panel ~~~~~~~~~~~~~~~~~~~~
 
 function openContactPanelContents() {
   console.log("open contact")
@@ -66,74 +171,21 @@ closeProjectsPanelContents();
 }
 
   if (getContactPanelContents.classList.contains("hidden")) {
-    // getContactPanelContents.style.display = "flex";
     getContactPanelContents.classList.remove("hidden");
     getContactPanelContents.classList.add("visible");
-    //     hide descriptor
-    // getMeDescriptor.style.display = "none";
-    //     make the excess page shrink
-    // document.getElementById("shrink").style.flexGrow = ".001";
+    hideMeSubtitle();
   } else {
     closeContactPanelContents();
+    showMeSubtitle();
   }
 }
+
 function closeContactPanelContents() {
-    // when adding to this function you should likely add it to other panel functions
-  // getContactPanelContents.style.animation = "1s slideOutContact";
-  // setTimeout(() => {
     getContactPanelContents.classList.remove("visible");
     getContactPanelContents.classList.add("hidden");
-  //     make the excess page grow
-    // document.getElementById("shrink").style.flexGrow = "1";
-    // getContactPanelContents.style.display = "none";
-    // getContactPanelContents.style.animation = "1s slideInContact";
-  // }, 995);
 }
 
 
 
 
-function openWaluigiContents() {
-hideFolders();
-  setTimeout (() => {
-showProjectContents();
-    }, 100)
-  getTitle.innerHTML = "Waluigi Jail";
-  getSubtitle.innerHTML = "This is an introductory web development project intended to develop HTML, CSS, and JavaScript skills. Users interact with the central image, triggering dynamic transformations and text display with each click, demonstrating foundational coding proficiency and creativity."
-  getBulletPoints.innerHTML = "123";
-  getLink.setAttribute("href", "kathryn.mckean.github.io/waluigijail/");
-}
-function openMemoryContents() {
-hideFolders();
-  setTimeout (() => {
-showProjectContents();
-     }, 100)
-  getTitle.innerHTML = "Memory Game";
-  getSubtitle.innerHTML = "This is a simple project centered around JavaScript, focusing on state management and the utilization of for loops."
-  getBulletPoints.innerHTML = "1234";
-  getLink.setAttribute("href", "kathryn.mckean.github.io/memorygame/");
-}
 
-function showFolders() {
-  console.log("go back to folder screen");
-  setTimeout (() => {
-  getFolders.classList.remove("hidden");
-  getFolders.classList.add("visible");
-          }, 300)
-  getProjectContents.classList.remove('visible');
-  getProjectContents.classList.add("hidden");
-}
-
-function hideFolders() {
- console.log("hiding folders");
- getFolders.classList.remove("visible");
- getFolders.classList.add("hidden");
-}
-
-let getProjectContents = document.getElementById("projectContentsFrame");
-
-function showProjectContents() {
-  console.log("showing content frame")    
-  getProjectContents.classList.remove('hidden');
-  getProjectContents.classList.add("visible");
-}
